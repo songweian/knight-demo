d4e0ec1920c4880175e0b6803b1e88b01453c2e5
weian404 <weian404@gmail.com>
2024-03-27 10:19:13 +0800

    init project

---
+++ backend/src/main/java/com/knight/demo/KnightDemoApplication.java
@@ -0,0 +1,13 @@
+package com.knight.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnightDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnightDemoApplication.class, args);
+	}
+
+}
